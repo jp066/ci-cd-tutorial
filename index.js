677412d4236458e63ea18afcf6640c50189196d5
joaomatos <joaomatos@brightbee.com.br>
2026-04-17 08:55:51 -0300

    mudança de nome da branch

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}!`);
 });
