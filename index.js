3b9229604965e2939149b60cb2562f8669fc5711
joaomatos <joaomatos@brightbee.com.br>
2026-04-17 10:29:01 -0300

    feat: .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require("dotenv").config;
 
 config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
