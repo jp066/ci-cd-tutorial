f06e71c50c0d5cadd4de3fc0ee59f8b01b7eb191
joaomatos <joaomatos@brightbee.com.br>
2026-04-18 12:05:58 -0300

    Change the content type response

--- app.js
+++ app.js
@@ -6,18 +6,9 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-type", "text/html");
-  res.send(`
-    <html>
-      <head>
-        <title>Node Js Web Serve Version 3</title>
-      </head>
-    </html>
-    <body>
-      <!-- <h1>Hello world! I'm a Node/Express Js web server version 2...</h1> -->
-      <h1>Hello world! I'm speaking with Mr. Ben!...</h1>
-    </body>
-    `);
+  res.setHeader("Content-type", "application/json");
+  const data = "João Pedro";
+  res.json({ message: `Hello, ${data}!` });
 });
 
 module.exports = app;
